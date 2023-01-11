cb3b5d6ee49b235ceec05ccc5e5d11901afdd841
Daniel Chesters <codeberg.org@coin-coin.xyz>
2023-01-11 23:46:07 +0100

    [Neovim] Disable check third party and telemetry in lsp lua config

--- nvim_init.lua
+++ nvim_init.lua
@@ -366,7 +366,13 @@ require('lspconfig').sumneko_lua.setup {
       diagnostics = {
         globals = { 'vim' },
       },
-      workspace = { library = vim.api.nvim_get_runtime_file('', true) },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file('', true),
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
+      },
     },
   },
 }
